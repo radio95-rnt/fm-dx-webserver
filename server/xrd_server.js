7ae8c4a24a7bdd8b8c8f4bcff7463d6f3161a8f6
KubaPro010 <kuba.pro010@gmail.com>
2026-04-04 22:06:42 +0200

    huh?

--- server/xrd_server.js
+++ server/xrd_server.js
@@ -1,5 +1,5 @@
 const storage = require('./storage');
-const datahandler = require('./datahandler');
+const { initialData } = require('./datahandler');
 const WebSocket = require('ws');
 const xrd = new WebSocket.Server({ noServer: true });
 
@@ -10,13 +10,13 @@ xrd.on('connection', (ws, request) => {
     return;
   }
 
-  ws.send(`o${datahandler.initialData.users},0\n`);
-  ws.send(`T${datahandler.initialData.freq * 1000}\n`);
-  ws.send(`G${datahandler.initialData.eq}${datahandler.initialData.ims}\n`);
-  ws.send(`Z${datahandler.initialData.ant}\n`);
-  ws.send(`A${datahandler.initialData.agc}\n`);
-  ws.send(`F${datahandler.initialData.bw}\n`);
-  ws.send(`W${datahandler.initialData.bw}\n`);
+  ws.send(`o${initialData.users},0\n`);
+  ws.send(`T${initialData.freq * 1000}\n`);
+  ws.send(`G${initialData.eq}${initialData.ims}\n`);
+  ws.send(`Z${initialData.ant}\n`);
+  ws.send(`A${initialData.agc}\n`);
+  ws.send(`F${initialData.bw}\n`);
+  ws.send(`W${initialData.bw}\n`);
   ws.send(`OK\n`);
 
   ws.on('message', (message) => storage.ctl_output.write(`${message}\n`));
