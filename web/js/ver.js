477a80045f656f96660b77e80cd7caeba33f95b6
KubaPro010 <kuba.pro010@gmail.com>
2026-04-11 13:45:44 +0200

    new version

--- web/js/ver.js
+++ web/js/ver.js
@@ -1,2 +1,2 @@
-const versionDate = new Date('Apr 3, 2026 22:30:00');
-const currentVersion = `v1.4.0a [${versionDate.getDate()}/${versionDate.getMonth() + 1}/${versionDate.getFullYear()}]`;
+const versionDate = new Date('Apr 11, 2026 13:30:00');
+const currentVersion = `v1.4.0b [${versionDate.getDate()}/${versionDate.getMonth() + 1}/${versionDate.getFullYear()}]`;
